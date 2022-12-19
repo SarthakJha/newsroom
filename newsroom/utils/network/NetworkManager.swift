2f615d925b1f37a157d193a07d2f172035baab01
Sarthak Jha <sarthakjha2001@gmail.com>
2022-12-19 15:46:42 +0530

    feat: search fetch call

--- newsroom/utils/network/NetworkManager.swift
+++ newsroom/utils/network/NetworkManager.swift
@@ -11,8 +11,9 @@ import Foundation
 final class NetworkManager {
     var baseURL: String?
     var apiKey: String?
-    fileprivate var apiVersion = "/v2"
-    fileprivate var topHeadlineRoute = "/top-headlines"
+    fileprivate let apiVersion = "/v2"
+    fileprivate let topHeadlineRoute = "/top-headlines"
+    fileprivate var everythingRoute = "/everything"
     
     init(baseURL:String, apiKey: String){
         self.baseURL = baseURL
@@ -20,22 +21,28 @@ final class NetworkManager {
     }
     
     // fetch headlines
+    /**
+     * default category= entertainment
+     */
     func fetchHeadlines(category cat: Category?, completion: @escaping((ArticleResponse?, Error?)-> Void)) {
+        
         guard var url: URL = URL(string: baseURL!+apiVersion+topHeadlineRoute) else {return}
         var topHeadlines: ArticleResponse?
         var queryItems: [URLQueryItem] = [URLQueryItem(name: "apiKey", value: Secrets.APIKey)]
         
         if let cat = cat {
             queryItems.append(URLQueryItem(name: "category", value: cat.rawValue))
+        }else{
+            queryItems.append(URLQueryItem(name: "category", value: "entertainment"))
         }
         
         url.append(queryItems: queryItems)
-        print(url.absoluteString)
 
         let dataSession = URLSession.shared.dataTask(with: URLRequest(url: url)) { (data, response, error) in
             
             if let error = error{
                 completion(nil,error)
+                return
             }
             guard let data = data else {return}
             do{
@@ -48,6 +55,33 @@ final class NetworkManager {
         }
         dataSession.resume()
         
+    }
+    
+    
+    // MARK :- put sources also
+    
+    func fetchSearchResults(searchText text: String ,Source completion: @escaping((ArticleResponse?,Error?) -> Void)){
+        guard var url: URL = URL(string: baseURL!+apiVersion+topHeadlineRoute) else {return}
+        var queryItems: [URLQueryItem] = [URLQueryItem(name: "apiKey", value: Secrets.APIKey)]
+        url.append(queryItems: queryItems)
+        
+        var searchResponse: ArticleResponse?
+        let dataSession = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error{
+                completion(nil, error)
+                return
+            }
+            guard let data = data else {return}
+
+            do{
+                searchResponse = try JSONDecoder().decode(ArticleResponse.self, from: data)
+                completion(searchResponse,nil)
+            }catch let error{
+                completion(nil, error)
+            }
+        }
+        
+        dataSession.resume()
     }
     
     
